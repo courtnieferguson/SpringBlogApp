f40a44c43a290d8d1af66947b03cf65565119536
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2021-09-28 14:08:35 -0500

    copy of project

--- src/main/java/com/codeup/example/PolarisSpringBlogApplication.java
+++ src/main/java/com/codeup/example/PolarisSpringBlogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBlogAppApplication {
+public class PolarisSpringBlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBlogAppApplication.class, args);
+        SpringApplication.run(PolarisSpringBlogApplication.class, args);
     }
 
 }
